13301012d9a00d302cad8976981030294ce56c2d
Aynaet <63262664+Aynaet@users.noreply.github.com>
2020-04-09 20:22:10 +0200

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,27 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+st='\033[0m' #Stop
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+
+
+#Mod by        : RyuXploit
+#Tool Unfaedah : Hehe;"v"
+
+clear
+            
+            echo $pur"  ╦ ╦╔╗╔╔═╗╔═╗╔═╗╔╦╗╔═╗╦ ╦   "      
+            echo $pur"  ║ ║║║║╠╣ ╠═╣║╣  ║║╠═╣╠═╣"
+            echo $pur"  ╚═╝╝╚╝╚  ╩ ╩╚═╝═╩╝╩ ╩╩ ╩"
+            echo $mer"  ×××Tool Unfaedah | RyuXploit $st"
