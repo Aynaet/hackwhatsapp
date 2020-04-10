8ddec93c8bbc9116b069ed4b066caa46942be048
Aynaet <63262664+Aynaet@users.noreply.github.com>
2020-04-10 12:38:57 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -12,3 +12,45 @@ clear
             echo $pur"  |  /  \  |  |        |       |          |         |   |      |   |           "
             echo $pur"  |_/    \_|  |______  |_____  |________  |_________|   |      |   |_______    "     
             echo $mer"                 WhatsappHack | Aynaet:Git                                     "
+
+
+clear
+echo $pur"             
+echo $pur"                     .ed"""" """$$$$be.
+echo $pur"                    -"           ^""**$$$e.
+echo $pur"                  ."                   '$$$c
+echo $pur"                 /                      "4$$b
+echo $pur"                d  3                     $$$$
+echo $pur"                $  *                   .$$$$$$
+echo $pur"               .$  ^c           $$$$$e$$$$$$$$.
+echo $pur"               d$L  4.         4$$$$$$$$$$$$$$b
+echo $pur"               $$$$b ^ceeeee.  4$$ECL.F*$$$$$$$
+echo $pur"   e$""=.      $$$$P d$$$$F $ $$$$$$$$$- $$$$$$
+echo $pur"  z$$b. ^c     3$$$F "$$$$b   $"$$$$$$$  $$$$*"      .=""$c
+echo $pur" 4$$$$L   \     $$P"  "$$b   .$ $$$$$...e$$        .=  e$$$.
+echo $pur" ^*$$$$$c  %..   *c    ..    $$ 3$$$$$$$$$$eF     zP  d$$$$$
+echo $pur"   "**$$$ec   "\   %ce""    $$$  $$$$$$$$$$*    .r" =$$$$P""
+echo $pur"        "*$b.  "c  *$e.    *** d$$$$$"L$$    .d"  e$$***"
+echo $pur"          ^*$$c ^$c $$$      4J$$$$$% $$$ .e*".eeP"
+echo $pur"             "$$$$$$"'$=e....$*$$**$cz$$" "..d$*"
+echo $pur"                "*$$$  *=%4.$ L L$ P3$$$F $$$P"
+echo $pur"                   "$   "%*ebJLzb$e$$$$$b $P"
+echo $pur"                     %..      4$$$$$$$$$$ "
+echo $pur"                      $$$e   z$$$$$$$$$$%
+echo $pur"                       "*$c  "$$$$$$$P"
+echo $pur"                       ."""*$$$$$$$$bc
+echo $pur"                     .-"    .$***$$$"""*e.
+echo $pur"                 .-"    .e$"     "*$c  ^*b.
+echo $pur"           .=*""""    .e$*"          "*bc  "*$e..
+echo $pur"         .$"        .z*"               ^*$e.   "*****e.
+echo $pur"         $$ee$c   .d"                     "*$.        3.
+echo $pur"         ^*$E")$..$"                         *   .ee==d%
+echo $pur"            $.d$$$*                           *  J$$$e*
+echo $pur" """""                             "$$$"
+
+echo ""
+echo -n $cy" Enter Whatsapp Number: "
+read t
+echo $pur"<•===========================•>"
+echo $me "Verification Code loading "
+sleep 7
