16fa88dc7310996d95626dc253dab15a3abfce0e
Aynaet <63262664+Aynaet@users.noreply.github.com>
2020-04-10 12:48:35 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,12 +6,12 @@
 
 clear
 
-            echo $pur"  |        |   ______  |        ________   _________    |\    /|    _______    "
-            echo $pur"  |        |  |        |       |          |         |   | \  / |   |           "
-            echo $pur"  |   /\   |  |______  |       |          |         |   |  \/  |   |_______    "
-            echo $pur"  |  /  \  |  |        |       |          |         |   |      |   |           "
-            echo $pur"  |_/    \_|  |______  |_____  |________  |_________|   |      |   |_______    "     
-            echo $mer"                 WhatsappHack | Aynaet:Git                                     "
+                                          
+echo $pur"  \ \  /\  / /___ | |  ___  ___   _ __ ___    ___     "
+echo $pur"   \ \/  \/ // _ \| | / __|/ _ \ | '_ ` _ \  / _ \    "
+echo $pur"    \  /\  /|  __/| || (__| (_) || | | | | ||  __/    "
+echo $pur"     \/  \/  \___||_| \___|\___/ |_| |_| |_| \___|    "
+echo $mer"               WhatsappHack | Aynaet:Git              "
 
 
 echo ""
@@ -20,3 +20,8 @@ read t
 echo $pur"<•===========================•>"
 echo $me "Verification Code loading "
 sleep 7
+mport random
+
+number = random.randint(1, 10)
+
+print("The Code is", number)
