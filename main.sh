dd64a211e5f4654a9f9666156ae1dfabd7da13e1
Aynaet <63262664+Aynaet@users.noreply.github.com>
2020-04-10 13:05:33 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -15,7 +15,10 @@ clear
  echo -n $cy" Enter Whatsapp Number: "
  read t
  echo $pur"<•===========================•>"
+ echo -n $cy" Click on verification on Whatsapp "
+ read t
+ echo $pur"<•===========================•>"
  echo $me "Verification Code loading "
- sleep 7
+ sleep 10
 
 echo $RANDOM
